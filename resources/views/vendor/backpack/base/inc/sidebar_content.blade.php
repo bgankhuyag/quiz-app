eec4440b7ef4b0ef04e41d5b079bf75c30aa84db
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-10 16:38:03 +0800

    edit

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }} dash</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class="nav-icon far fa-user"></i> Users</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('answer') }}'><i class="nav-icon fas fa-key"></i> Answers</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class="nav-icon fas fa-th"></i> Categories</a></li>
