a5ad4f855ab5abef21b35dfc59a0caf09c83c5d7
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-10 12:03:48 +0800

    db

--- config/database.php
+++ config/database.php
@@ -51,14 +51,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('CLEARDB_DATABASE_URL'),
             'port' => env('DB_PORT', '3306'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
