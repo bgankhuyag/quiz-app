d331521decdaa6531a3e1b821d06ecc8e551cdb5
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-10 17:21:31 +0800

    edit

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -66,8 +66,8 @@ class CheckIfAdmin
         // dd(backpack_user()['roles_id'] != Roles::firstWhere('role', 'admin')['id']);
         if (! $this->checkIfUserIsAdmin(backpack_user())) {
           // dd('here');
-          return backpack_url('login');
-          // return $this->respondToUnauthorizedRequest($request);
+          // return backpack_url('login');
+          return $this->respondToUnauthorizedRequest($request);
         }
         // dd("here");
         if (backpack_user()['roles_id'] != Roles::firstWhere('role', 'admin')['id']) {
