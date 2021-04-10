ecb47d1e33f1996975f2fc23fb66fd988441b0b8
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-10 16:00:27 +0800

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,9 +103,8 @@ function checkPage(page) {
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-              hihihi
                 <div class="navbar-brand" href="">
-                    {{ config('app.name', 'Laravel') }} hihih
+                    {{ config('app.name', 'Laravel') }}
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
