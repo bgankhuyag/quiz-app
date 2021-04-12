4f1f6cb5d30bfed96b3381d82acb77a54b0ecd88
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-12 15:15:47 +0800

    edit

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,22 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="links">
-  <div class="list-group">
-    <a class="list-group-item list-group-item-action" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("user") }}'><i class="nav-icon far fa-user"></i> Users</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("answer") }}'><i class="nav-icon fas fa-key"></i> Answers</a>
-    <a class='list-group-item list-group-item-action active' href='{{ route("category") }}'><i class="nav-icon fas fa-th"></i> Categories</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("subcategory") }}'><i class="nav-icon fas fa-layer-group"></i> Sub-Categories</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("image") }}'><i class="nav-icon fas fa-images"></i> Images</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("option") }}'><i class="nav-icon fas fa-bars"></i> Options</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("question") }}'><i class="fas fa-question-circle"></i> Questions</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("role") }}'><i class="nav-icon fas fa-user-shield"></i> Roles</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("selected") }}'><i class="nav-icon fas fa-check-circle"></i> Selected Options</a>
-    <a class='list-group-item list-group-item-action' href='{{ route("points") }}'><i class="nav-icon fas fa-images"></i> Points</a>
-  </div>
-</div> -->
 <div class="container">
+  <a href="{{route('addCategoryPage')}}"><button type="button" class="btn btn-info">Add</button></a>
   <h3>Categories</h3>
   <table class="table table-striped">
   <thead>
