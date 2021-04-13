73410e14759c0ed362bc59af0f83e9cd3abac200
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-13 17:19:16 +0800

    edit

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -195,7 +195,7 @@ return [
 
     // Set this to false if you would like to skip adding the password recovery routes
     // (you then need to manually define the routes in your web.php)
-    'setup_password_recovery_routes' => true,
+    'setup_password_recovery_routes' => false,
 
     // Set this to false if you would like to skip adding the dashboard routes
     // (you then need to overwrite the login route on your AuthController)
@@ -203,7 +203,7 @@ return [
 
     // Set this to false if you would like to skip adding "my account" routes
     // (you then need to manually define the routes in your web.php)
-    'setup_my_account_routes' => true,
+    'setup_my_account_routes' => false,
 
     /*
     |--------------------------------------------------------------------------
