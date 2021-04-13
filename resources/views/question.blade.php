7aced51c3cbf5f99668851a473a9267bdce25a50
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-13 16:37:33 +0800

    edit

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -11,9 +11,35 @@
   @endif
   <a href="{{route('addQuestionPage')}}"><button style="margin-bottom: 20px;" type="button" class="btn btn-primary">Add</button></a>
   <h3>Questions</h3>
-  <div class="table-responsive">
-  <table class="table table-striped">
-  <thead>
+  <div class="table-responsive-lg">
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col-sm">ID</th>
+        <th scope="col">Question</th>
+        <th scope="col">Category ID</th>
+        <th scope="col">Sub-Category ID</th>
+        <th scope="col">Correct Option ID</th>
+        <th scope="col">Image</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($questions as $question)
+      <tr>
+        <td>{{$question->id}}</td>
+        <td>{{$question->question}}</td>
+        <td>{{$question->categories_id}}</td>
+        <td>{{$question->sub_categories_id}}</td>
+        <td>{{$question->correct_option_id}}</td>
+        <td>{{$question->getRawOriginal('image')}}</td>
+        <td class="float-right">
+          <a href="{{route('editQuestionPage', ['id' => $question->id])}}"><button type="button" class="btn btn-outline-primary btn-sm">Edit</button></a>
+          <a href="{{route('removeQuestion', ['id' => $question->id])}}"><button type="button" class="btn btn-outline-danger btn-sm"><i class="far fa-trash-alt"></i> Delete</button></a>
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
     <tr>
       <th scope="col">ID</th>
       <th scope="col">Question</th>
@@ -23,34 +49,8 @@
       <th scope="col">Image</th>
       <th></th>
     </tr>
-  </thead>
-  <tbody>
-    @foreach($questions as $question)
-    <tr>
-      <td>{{$question->id}}</td>
-      <td>{{$question->question}}</td>
-      <td>{{$question->categories_id}}</td>
-      <td>{{$question->sub_categories_id}}</td>
-      <td>{{$question->correct_option_id}}</td>
-      <td>{{$question->getRawOriginal('image')}}</td>
-      <td class="float-right">
-        <a href="{{route('editQuestionPage', ['id' => $question->id])}}"><button type="button" class="btn btn-outline-primary btn-sm">Edit</button></a>
-        <a href="{{route('removeQuestion', ['id' => $question->id])}}"><button type="button" class="btn btn-outline-danger btn-sm"><i class="far fa-trash-alt"></i> Delete</button></a>
-      </td>
-    </tr>
-    @endforeach
-  </tbody>
-  <tr>
-    <th scope="col">ID</th>
-    <th scope="col">Question</th>
-    <th scope="col">Category ID</th>
-    <th scope="col">Sub-Category ID</th>
-    <th scope="col">Correct Option ID</th>
-    <th scope="col">Image</th>
-    <th></th>
-  </tr>
-  </table>
-</div>
+    </table>
+  </div>
   {{ $questions->links() }}
 </div>
 
