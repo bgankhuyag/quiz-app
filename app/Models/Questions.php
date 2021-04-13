6bfed74cc3ba7ccb59330974edb0b0261ebd2047
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-13 14:54:50 +0800

    edit

--- app/Models/Questions.php
+++ app/Models/Questions.php
@@ -4,6 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Questions extends Model
 {
