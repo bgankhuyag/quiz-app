2486fad633575946c3f7419f902d710b18a5a8cb
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-20 15:33:37 +0800

    edit

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -93,6 +93,9 @@ class QuizController extends Controller
     public function leaderboard(Request $request, $id) {
       $points = Points::where('categories_id', $id)->orderBy('points', 'desc')->join('users', 'points.users_id', '=', 'users.id')->take(5)->get(['name', 'points.id', 'points']);
       $user_point = Points::where('categories_id', $id)->where('users_id', auth()->id())->join('users', 'points.users_id', '=', 'users.id')->get(['users_id', 'name', 'points.id', 'points']);
+      if ($user_point == null) {
+        $user_point = [];
+      }
       $result = ['data' => $points, 'user' => $user_point];
       return response()->json($result);
     }
